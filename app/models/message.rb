3735f4812123f0ceb28ff7498c9a60728a386d16
Alyson Takai <shtakai@gmail.com>
2016-06-09 20:32:23 -0700

    add User 1-* Message

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,6 @@
 class Message < ActiveRecord::Base
   belongs_to :post
+  belongs_to :user
 
   validates :author, :message, presence: true
   validates :message, length: {minimum: 15}
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160610032406) do
+ActiveRecord::Schema.define(version: 20160610033104) do
 
   create_table "blogs", force: :cascade do |t|
     t.string   "name"
@@ -26,9 +26,11 @@ ActiveRecord::Schema.define(version: 20160610032406) do
     t.text     "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   add_index "messages", ["post_id"], name: "index_messages_on_post_id"
+  add_index "messages", ["user_id"], name: "index_messages_on_user_id"
 
   create_table "owners", force: :cascade do |t|
     t.integer  "user_id"
