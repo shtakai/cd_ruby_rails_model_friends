513f1b217ceb4991c64313b899d7ca04cd7dcc70
Alyson Takai <shtakai@gmail.com>
2016-06-09 22:30:02 -0700

    implemented Friendship

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ActiveRecord::Base
   has_many :posts
   has_many :blogs, through: :owners
   has_many :comments, as: :commentable
+  has_many :friendships
+  has_many :friends, through: :friendships
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160610045411) do
+ActiveRecord::Schema.define(version: 20160610051116) do
 
   create_table "blogs", force: :cascade do |t|
     t.string   "name"
@@ -29,6 +29,16 @@ ActiveRecord::Schema.define(version: 20160610045411) do
   end
 
   add_index "comments", ["commentable_id"], name: "index_comments_on_commentable_id"
+
+  create_table "friendships", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "friend_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "friendships", ["friend_id"], name: "index_friendships_on_friend_id"
+  add_index "friendships", ["user_id"], name: "index_friendships_on_user_id"
 
   create_table "messages", force: :cascade do |t|
     t.integer  "post_id"
