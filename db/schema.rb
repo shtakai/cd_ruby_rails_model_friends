c741013264137318147a417b880c5ce85c99fa0b
Alyson Takai <shtakai@gmail.com>
2016-06-09 21:56:34 -0700

    create Comment

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160610033104) do
+ActiveRecord::Schema.define(version: 20160610045411) do
 
   create_table "blogs", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 20160610033104) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "comment"
+    t.integer  "commentable_id"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
+
+  add_index "comments", ["commentable_id"], name: "index_comments_on_commentable_id"
 
   create_table "messages", force: :cascade do |t|
     t.integer  "post_id"
